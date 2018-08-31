b42d62aec130f7e0179bf2caa70831dd0ef783b1
hyz <609927332@qq.com>
2018-08-31 22:27:19 +0800

    新增初始化数据库的sql

---
+++ sql/init.sql
@@ -0,0 +1,158 @@
+/*
+SQLyog v10.2 
+MySQL - 5.7.22 : Database - yazhe_web
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`yazhe_web` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `yazhe_web`;
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `id` varchar(32) NOT NULL DEFAULT '' COMMENT '权限id',
+  `resource` varchar(32) NOT NULL DEFAULT '' COMMENT '权限对应的资源',
+  `resource_name` varchar(128) NOT NULL DEFAULT '' COMMENT '资源的中文名，前端展示',
+  `permission_code` varchar(128) NOT NULL DEFAULT '' COMMENT '权限的代码/通配符,对应代码中@RequiresPermissions 的value',
+  `permission_name` varchar(128) NOT NULL DEFAULT '' COMMENT '权限的中文释义',
+  `url` varchar(128) NOT NULL COMMENT '对应的url',
+  `required` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否本菜单必选权限,通常是"列表"权限是必选',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
+
+/*Data for the table `permission` */
+
+insert  into `permission`(`id`,`resource`,`resource_name`,`permission_code`,`permission_name`,`url`,`required`) values ('0','user','用户','user:add','添加','',0),('1','user','用户','user:delete','删除','',0),('10','brand','品牌','brand:delete','删除','',0),('11','brand','品牌','brand:update','更新','',0),('12','brand','品牌','brand:get','查询','',0),('13','location','地点','location:add','添加','',0),('14','location','地点','location:get','查询','',0),('15','location','地点','location:update','更新','',0),('16','location','地点','location:delete','删除','',0),('2','user','用户','user:update','更新','',0),('3','user','用户','user:get','查询','',0),('4','role','角色','role:get','查询','/user-auth/role',0),('5','role','角色','role:delete','删除','',0),('6','role','角色','role:update','更新','',0),('7','role','角色','role:add','添加','',0),('8','permission','权限','permission:get','查询','',0),('9','brand','品牌','brand:add','添加','',0);
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` varchar(32) NOT NULL DEFAULT '' COMMENT '角色id',
+  `name` varchar(32) NOT NULL DEFAULT '' COMMENT '角色名',
+  `locked` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否锁定',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`,`locked`,`create_time`,`update_time`) values ('1','管理员',0,'2018-05-13 17:11:42','2018-05-13 17:11:38');
+
+/*Table structure for table `role_permission` */
+
+DROP TABLE IF EXISTS `role_permission`;
+
+CREATE TABLE `role_permission` (
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `role_id` varchar(32) NOT NULL DEFAULT '' COMMENT '角色id',
+  `permission_id` varchar(32) NOT NULL DEFAULT '' COMMENT '权限id',
+  `locked` tinyint(1) NOT NULL DEFAULT '0' COMMENT '锁定',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_role_id` (`role_id`),
+  KEY `idx_permission_id` (`permission_id`),
+  CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色权限关系表';
+
+/*Data for the table `role_permission` */
+
+insert  into `role_permission`(`id`,`role_id`,`permission_id`,`locked`,`create_time`) values ('1','1','0',0,'2018-05-13 17:54:04'),('10','1','15',0,'2018-08-01 06:59:23'),('11','1','16',0,'2018-08-01 06:59:27'),('2','1','1',0,'2018-05-13 17:54:04'),('3','1','2',0,'2018-05-13 17:54:04'),('4','1','3',0,'2018-05-13 17:54:04'),('5','1','8',0,'2018-05-18 13:55:41'),('6','1','4',0,'2018-05-18 14:09:20'),('7','1','12',0,'2018-07-31 01:19:13'),('8','1','13',0,'2018-08-01 06:25:53'),('9','1','14',0,'2018-08-01 06:59:18');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `username` varchar(32) NOT NULL DEFAULT '' COMMENT '用户名',
+  `real_name` varchar(32) NOT NULL DEFAULT '' COMMENT '真实姓名',
+  `email` varchar(32) DEFAULT '' COMMENT '邮箱',
+  `phone` varchar(16) DEFAULT '' COMMENT '电话',
+  `address` varchar(64) DEFAULT '' COMMENT '住址',
+  `locked` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否锁定',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `last_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后一次登录时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`real_name`,`email`,`phone`,`address`,`locked`,`create_time`,`update_time`,`last_time`) values ('1526467363362171844','hyz','黄雅哲','609927332@qq.com','18728193205',' ',0,'2018-05-16 18:42:43','2018-08-31 03:45:14','2018-08-31 11:46:12'),('1532999182292648171','test','zzz','609927332@qq.com','11111111111','aaa',0,'2018-07-31 01:05:09','2018-07-31 01:05:09','2018-07-31 01:05:09');
+
+/*Table structure for table `user_auth` */
+
+DROP TABLE IF EXISTS `user_auth`;
+
+CREATE TABLE `user_auth` (
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `user_id` varchar(32) NOT NULL DEFAULT '' COMMENT '用户id',
+  `identify_type` tinyint(3) NOT NULL DEFAULT '0' COMMENT '登录认证类型,0：用户名密码登录，1：微信登录',
+  `identifier` varchar(128) NOT NULL DEFAULT '' COMMENT '标识（手机号 邮箱 用户名或第三方应用的唯一标识）',
+  `credential` varchar(128) NOT NULL DEFAULT '' COMMENT '登录凭证',
+  `locked` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否锁定',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+  CONSTRAINT `user_auth_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户登录认证表';
+
+/*Data for the table `user_auth` */
+
+insert  into `user_auth`(`id`,`user_id`,`identify_type`,`identifier`,`credential`,`locked`,`create_time`,`update_time`) values ('1526467363565941170','1526467363362171844',0,'hyz','123456',0,'2018-05-16 18:42:43','2018-05-16 18:42:43'),('1532999182370532626','1532999182292648171',0,'test','123456',0,'2018-07-31 01:05:09','2018-07-31 01:05:09');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `id` varchar(32) NOT NULL DEFAULT '',
+  `user_id` varchar(32) NOT NULL DEFAULT '' COMMENT '用户id',
+  `role_id` varchar(32) NOT NULL DEFAULT '' COMMENT '角色id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_role_id` (`role_id`),
+  CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户角色表';
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`id`,`user_id`,`role_id`,`create_time`) values ('1526467363636855881','1526467363362171844','1','2018-05-16 18:42:43'),('1532999182464614155','1532999182292648171','1','2018-07-31 01:05:09');
+
+/*Table structure for table `web_statistic` */
+
+DROP TABLE IF EXISTS `web_statistic`;
+
+CREATE TABLE `web_statistic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `clicks` bigint(20) NOT NULL DEFAULT '0' COMMENT '点击量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `web_statistic` */
+
+insert  into `web_statistic`(`id`,`clicks`) values (1,3),(2,1),(3,4),(4,2),(5,4),(6,0),(7,3),(8,1),(9,3),(10,0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
